5c7d31207e5b1e35e6bf1ed093fee2b49f0cc7e9
Yuri M <yurisdraughts@gmail.com>
2022-08-04 21:24:24 +0300

    added 3rd task and some debug files

---
+++ task003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0 || n > 7) Console.WriteLine("Это не номер дня недели!");
+else if (n == 1) Console.WriteLine("Понедельник.");
+else if (n == 2) Console.WriteLine("Вторник.");
+else if (n == 3) Console.WriteLine("Среда.");
+else if (n == 4) Console.WriteLine("Четверг.");
+else if (n == 5) Console.WriteLine("Пятница.");
+else if (n == 6) Console.WriteLine("Суббота.");
+else if (n == 7) Console.WriteLine("Воскресенье.");
