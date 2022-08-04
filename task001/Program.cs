01632f3d1ae141e6aa18fa6a90dc454cfb91a046
Yuri M <yurisdraughts@gmail.com>
2022-08-04 20:45:47 +0300

    Added 1st task

---
+++ task001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.Write("Квадрат введённого вами числа равен ");
+Console.WriteLine(result);
