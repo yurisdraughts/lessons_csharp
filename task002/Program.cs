3af47e9709b493e86ec5175d239c99374358eee8
Yuri M <yurisdraughts@gmail.com>
2022-08-04 20:59:39 +0300

    added 2nd task

---
+++ task002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b) 
+{
+    Console.WriteLine("Да. Первое число является квадратом второго.");
+}
+else
+{
+    
+    Console.WriteLine("Нет. Первое число не является квадратом второго.");
+}
