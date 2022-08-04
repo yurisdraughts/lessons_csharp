d2fe286a1a37e8b3be6a101551e58126ae1e423a
Yuri M <yurisdraughts@gmail.com>
2022-08-04 22:07:23 +0300

    Added 5th task

---
+++ task005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    int digit = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number} равна {digit}.");
+}
+else
+{
+    Console.WriteLine("Введённое число не трёхзначное.");
+}
