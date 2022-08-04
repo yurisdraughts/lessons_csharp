7f0ffe0a776114507435803274eb7cb63cd9786a
Yuri M <yurisdraughts@gmail.com>
2022-08-04 21:56:42 +0300

    Added 4th task

---
+++ task004/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = -1 * num; i <= num; i++)
+{
+    Console.Write(i.ToString() + " ");
+}
