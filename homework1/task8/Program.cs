614e42e672405b8d64be69d0b7fe9e2c4adffb46
Yuri M <yurisdraughts@gmail.com>
2022-08-05 09:34:17 +0300

    Made task8 work for numbers less then 2

--- homework1/task8/Program.cs
+++ homework1/task8/Program.cs
@@ -1,11 +1,31 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.Write("Введите положительное число: "); //или для отрицательных программа тоже должна работать?
+Console.Write("Введите число: ");
 double n = Convert.ToDouble(Console.ReadLine());
 
-Console.Write($"Все чётные числа от 1 до {n}: ");
-int i = 2;
-while (i <= n)
+
+if (n >= 2)
 {
-    Console.Write(i.ToString() + " ");
-    i += 2;
+    Console.Write($"Все чётные числа от 1 до {n}: ");
+
+    int i = 2;
+    while (i <= n)
+    {
+        Console.Write(i.ToString() + " ");
+        i += 2;
+    }
+}
+else if (n <= 0)
+{
+    Console.Write($"Все чётные числа от 1 до {n}: ");
+    
+    int i = 0;
+    while (i >= n)
+    {
+        Console.Write(i.ToString() + " ");
+        i -= 2;
+    }
+}
+else
+{
+    Console.Write($"Между 1 и {n} нет чётных чисел.");
 }
