def708b770f5cec9116f6897e436eb594e79574a
Yuri M <yurisdraughts@gmail.com>
2022-08-08 20:54:10 +0300

    Added sem2/task004

---
+++ sem2/task004/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"{num} -> да, число кратно одновременно 7 и 23");
+else Console.WriteLine($"{num} -> нет, число не кратно одновременно 7 и 23");
