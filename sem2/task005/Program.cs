2abcdc48628a6431ed0140a0fe8e51c725b79f1b
Yuri M <yurisdraughts@gmail.com>
2022-08-08 21:01:02 +0300

    Added sem2/task005

---
+++ sem2/task005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое целое число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+double b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || b * b == a) Console.WriteLine($"{a}, {b} -> да, одно из чисел является квадратом другого");
+else Console.WriteLine($"{a}, {b} -> нет, ни одно из чисел не является квадратом другого");
