9ef2d906a70e0880593a7ac3131e7bd938433125
Yuri M <yurisdraughts@gmail.com>
2022-08-08 21:08:37 +0300

    modified:   sem2/task003

--- sem2/task003/Program.cs
+++ sem2/task003/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Write("Введите первое целое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
+Console.Write("Введите второе целое число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num2 == 0) Console.WriteLine("Невозможно разделить на 0");
