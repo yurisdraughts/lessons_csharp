ccc6930d2d377de0f6522d206203f0a739e591b9
Yuri M <yurisdraughts@gmail.com>
2022-08-08 20:27:10 +0300

    Added sem2/task001

---
+++ sem2/task001/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = new Random().Next(10, 100); //случайное число в диапазоне [10; 100)
+int digit1 = number / 10;
+int digit2 = number % 10;
+if (digit1 > digit2) Console.WriteLine($"{number}, {digit1}");
+else Console.WriteLine($"{number}, {digit2}");
