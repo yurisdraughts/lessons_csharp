391088c21f92e98ca6885c4da662b4e7d6a2ca95
Yuri M <yurisdraughts@gmail.com>
2022-08-09 11:14:46 +0300

    Added homework2/task13_v2

---
+++ homework2/task13_v2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100)
+{
+    int n = number;
+    while (n >= 1000) n /= 10;
+
+    int digit = n % 10;
+    Console.WriteLine($"{number} -> {digit}");
+}
+else Console.WriteLine($"{number} -> третьей цифры нет");
