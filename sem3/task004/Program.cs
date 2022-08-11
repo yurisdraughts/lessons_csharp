e0e095f73a02a4b72791c131a34aa39409502d0c
Yuri M <yurisdraughts@gmail.com>
2022-08-11 21:58:46 +0300

    Added sem3/task004/

---
+++ sem3/task004/Program.cs
@@ -0,0 +1,16 @@
+﻿void Squares(int n)
+{
+    Console.Write($"{n} -> ");
+
+    int i = 1;
+    while (i < n)
+    {
+        Console.Write($"{i * i}, ");
+        i++;
+    }
+
+    Console.WriteLine($"{n * n}.");
+}
+
+Squares(5);
+Squares(2);
