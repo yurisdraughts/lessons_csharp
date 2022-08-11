daec38f44dd87594841c3e34d44204e1c352fc6e
Yuri M <yurisdraughts@gmail.com>
2022-08-11 20:46:02 +0300

    Added sem3\task001

---
+++ sem3/task001/Program.cs
@@ -0,0 +1,23 @@
+﻿int FindQuarter(double x, double y)
+{
+    if (x == 0 || y == 0) return 0;
+    else
+    {
+        if (x > 0)
+        {
+            if (y > 0) return 1;
+            else return 4;
+        }
+        else
+        {
+            if (y > 0) return 2;
+            else return 3;
+        }
+    }
+}
+
+Console.WriteLine(FindQuarter(4, 7));
+Console.WriteLine(FindQuarter(-19, 7));
+Console.WriteLine(FindQuarter(-1, -3));
+Console.WriteLine(FindQuarter(4, -8));
+Console.WriteLine(FindQuarter(0, 5));
