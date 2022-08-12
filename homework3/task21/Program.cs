928807e1418c0b1d0efdd9e35a6678bf1a5b17ee
Yuri M <yurisdraughts@gmail.com>
2022-08-12 10:05:04 +0300

    Added homework3/task21

---
+++ homework3/task21/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double CalcDistance3d(double[] A, double[] B)
+{
+    double distance = Math.Sqrt(Math.Pow(A[0] - B[0], 2) + Math.Pow(A[1] - B[1], 2) + Math.Pow(A[2] - B[2], 2));
+    return distance;
+}
+
+void PrintDistance3d(double[] A, double[] B)
+{
+    Console.WriteLine($"A({A[0]}, {A[1]}, {A[2]}); B({B[0]}, {B[1]}, {B[2]}) -> {CalcDistance3d(A, B)}");
+}
+
+PrintDistance3d(new double[] {3, 6, 8}, new double[] {2, 1, -7});
+PrintDistance3d(new double[] {7, -5, 0}, new double[] {1, -1, 9});
