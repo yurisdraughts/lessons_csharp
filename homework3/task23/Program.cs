e326a53ca72015fdeb50a726aaaefb42e8491b74
Yuri M <yurisdraughts@gmail.com>
2022-08-12 10:12:21 +0300

    Added homework3/task23

---
+++ homework3/task23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cubes(int n)
+{
+    Console.Write($"{n} -> ");
+
+    int i = 1;
+    while (i <= n)
+    {
+        Console.Write($"{Math.Pow(i, 3)}");
+        if (i != n) Console.Write(", ");
+        else Console.WriteLine("");
+        
+        i++;
+    }
+}
+
+Cubes(3);
+Cubes(5);
