297cf75e457957983f53dae0119a8f3e91fcff2c
Yuri M <yurisdraughts@gmail.com>
2022-08-15 20:33:27 +0300

    Added sem4\task002

---
+++ sem4/task002/Program.cs
@@ -0,0 +1,32 @@
+﻿int Digits(int number)
+{
+    int result = Math.Abs(number).ToString().Length;
+    return result;
+}
+
+int Digitsv2 (int number)
+{
+    int n = number;
+    int result = 1;
+
+    while (n > 10)
+    {
+        n /= 10;
+        result++;
+    }
+
+    return result;
+}
+
+void PrintDigits(int number)
+{
+    Console.WriteLine($"Число {number} содержит {Digits(number)} цифр.");
+}
+
+void PrintDigitsv2(int number)
+{
+    Console.WriteLine($"Число {number} содержит {Digitsv2(number)} цифр.");
+}
+
+PrintDigits(456);
+PrintDigitsv2(89126);
