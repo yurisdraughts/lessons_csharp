0987adb1e21e47fcf1c6a5571ed44ab9ef05140d
Yuri M <yurisdraughts@gmail.com>
2022-08-15 21:05:22 +0300

    Added sem4/task003

---
+++ sem4/task003/Program.cs
@@ -0,0 +1,17 @@
+﻿int Factorial(int number)
+{
+    int result = 1;
+    for(int i = 2; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+void PrintFactorial(int number)
+{
+    Console.WriteLine($"Факториал числа {number} равен {Factorial(number)}.");
+}
+
+PrintFactorial(4);
+PrintFactorial(5);
