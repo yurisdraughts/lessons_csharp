e6befb26701d6163d280c0ef65db0e35ddde9834
Yuri M <yurisdraughts@gmail.com>
2022-08-15 20:20:37 +0300

    Added sem4\task001

---
+++ sem4/task001/Program.cs
@@ -0,0 +1,18 @@
+﻿int Sum(int a)
+{
+    int result = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        result += i;
+    }
+
+    return result;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}.");
+}
+
+PrintSum(5);
