f30f0dbcd56c0458c2f0bf1d347277e1120aae1a
Yuri M <yurisdraughts@gmail.com>
2022-08-15 21:05:55 +0300

    Added sem4/task004

---
+++ sem4/task004/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] RandomBinaryArray()
+{
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray()
+{
+    int[] array = RandomBinaryArray();
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray();
+PrintArray();
+PrintArray();
