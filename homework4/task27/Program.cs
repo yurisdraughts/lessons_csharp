04590f0a88b08e9347a43a2f1ef3143a9203a747
Yuri M <yurisdraughts@gmail.com>
2022-08-16 08:20:50 +0300

    Added homework4/task27/

---
+++ homework4/task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DigitSum(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+void PrintDigitSum(int number)
+{
+    Console.WriteLine($"Сумма цифр числа {number} равна {DigitSum(number)}.");
+}
+
+PrintDigitSum(452);
+PrintDigitSum(82);
+PrintDigitSum(9012);
