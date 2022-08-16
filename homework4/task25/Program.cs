7700c3b3534a3c41fa5371773d7fc767cfbf7450
Yuri M <yurisdraughts@gmail.com>
2022-08-16 09:00:44 +0300

    Added homework4/task29/

--- homework4/task25/Program.cs
+++ homework4/task25/Program.cs
@@ -15,6 +15,6 @@ void PrintPow(double A, int B)
     Console.WriteLine($"{A} в степени {B} равно {Pow(A, B)}.");
 }
 
-PrintPow(3, 5); //3 в степени 5 равно 243.
-PrintPow(2, 4); //2 в степени 4 равно 16.
-PrintPow(1.5, 2); //1,5 в степени 2 равно 2,25.
+PrintPow(3, 5);
+PrintPow(2, 4);
+PrintPow(1.5, 2);
---
+++ homework4/task29/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// прежположим, что под элементами подразумеваются только целые числа - как в примере
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+// 1-й вариант: массив введённых пользователем чисел
+int[] InputArray8()
+{
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е целое число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+// 2-й вариант: массив случайных чисел в диапазоне [0; ceiling)
+// или (-ceiling; ceiling) в зависимости от значения 3-го аргумента
+int[] RandomArray8(int ceiling, bool includeNegatives)
+{
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, ceiling);
+        if (includeNegatives)
+        {
+            int n = new Random().Next(0, 2);
+            if (n == 0) array[i] *= -1;
+        }
+    }
+    return array;
+}
+
+//ВЫЗОВ МЕТОДОВ
+PrintArray(InputArray8());
+
+PrintArray(RandomArray8(45678, false));
+PrintArray(RandomArray8(2, false));
+PrintArray(RandomArray8(33, true));
