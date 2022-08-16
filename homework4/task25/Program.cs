15aff23e9165a02ccc0ffa39a519cffb6e4f4503
Yuri M <yurisdraughts@gmail.com>
2022-08-16 08:13:08 +0300

    Added homework4/task25

---
+++ homework4/task25/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double Pow(double A, int B)
+{
+    double result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+void PrintPow(double A, int B)
+{
+    Console.WriteLine($"{A} в степени {B} равно {Pow(A, B)}.");
+}
+
+PrintPow(3, 5); //3 в степени 5 равно 243.
+PrintPow(2, 4); //2 в степени 4 равно 16.
+PrintPow(1.5, 2); //1,5 в степени 2 равно 2,25.
