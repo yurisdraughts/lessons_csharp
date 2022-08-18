8ee8e6aa113171df86fce2be30451ff4b0b09520
Yuri M <yurisdraughts@gmail.com>
2022-08-18 20:31:25 +0300

    Added sem5/task001

---
+++ sem5/task001/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int FindPositiveSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum += array[i];
+    }
+    return sum;
+}
+
+int FindNegativeSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArray(12, -9, 9);
+PrintArray(array);
+Console.WriteLine($"Сумма положительных чисел равна {FindPositiveSum(array)}.");
+Console.WriteLine($"Сумма отрицательных чисел равна {FindNegativeSum(array)}.");
