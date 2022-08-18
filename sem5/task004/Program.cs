a4754d7156dd32b5dbb39ef2854c73239d076a24
Yuri M <yurisdraughts@gmail.com>
2022-08-18 22:06:04 +0300

    Added sem5/task004

---
+++ sem5/task004/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    string printedArray = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) printedArray += $"{array[i]}, ";
+        else printedArray += $"{array[i]}]";
+    }
+    Console.WriteLine(printedArray);
+}
+
+string Decline(int number)
+{
+    int two = number % 100;
+    int one = number % 10;
+    if (two != 11 && one == 1) return "число";
+    if ((two >= 12 && two <= 19) || one >= 5 || one == 0) return "чисел";
+    else return "числа";
+}
+
+void CountNumbersInRange(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max) count++;
+    }
+    PrintArray(array);
+    Console.WriteLine($"В массиве {count} {Decline(count)} в отрезке [{min}, {max}].");
+}
+
+int[] array = CreateArray(123, 0, 123);
+CountNumbersInRange(array, 10, 99);
