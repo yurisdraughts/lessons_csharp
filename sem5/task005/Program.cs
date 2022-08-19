018b601df854fd36d08826c42fce35aea68b2648
Yuri M <yurisdraughts@gmail.com>
2022-08-19 09:22:14 +0300

    Added comments to sem5/task005

--- sem5/task005/Program.cs
+++ sem5/task005/Program.cs
@@ -1,4 +1,12 @@
-﻿int[] CreateArray(int length, int min, int max)
+﻿//Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+//Результат запишите в новом массиве.
+
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+//зададим метод, создающий массив случайных чисел заданной длины в заданном диапазоне
+int[] CreateArray(int length, int min, int max)
 {
     int[] array = new int[length];
     for (int i = 0; i < length; i++)
@@ -8,18 +16,20 @@
     return array;
 }
 
+//зададим метод, приниамющий массив и возвращающий представление массива в виде строки
 string StringifyArray(int[] array)
 {
-    string stringArray = "[";
+    string stringArray = "";
     for (int i = 0; i < array.Length; i++)
     {
-        if (i != array.Length - 1) stringArray += $"{array[i]}, ";
-        else stringArray += $"{array[i]}]";
+        if (i != array.Length - 1) stringArray += $"{array[i]} ";
+        else stringArray += $"{array[i]}";
     }
     return stringArray;
 }
 
-void MultiplyArray(int[] array)
+//зададим метод, принимающий массив и возвращающий массив из произведений пар чисел
+int[] MultiplyArray(int[] array)
 {
     int l = array.Length;
     int[] newArray = new int[(l + 1)/2];
@@ -31,8 +41,21 @@ void MultiplyArray(int[] array)
 
     if (l % 2 == 1) newArray[newArray.Length - 1] = array[l / 2];
 
-    Console.WriteLine($"{StringifyArray(array)} -> {StringifyArray(newArray)}");
+    return newArray;
 }
 
-int[] array = CreateArray(5, 0, 10);
-MultiplyArray(array);
+
+//зададим метод, принимающий массив и выводящий на консоль исходный массив и массив произведений пар чисел
+void PrintResult(int[] array)
+{
+    int[] newArray = MultiplyArray(array);
+    Console.WriteLine($"[{StringifyArray(array)}] -> {StringifyArray(newArray)}");
+}
+
+//вызовем 2 раза (для чётных и нечётных длин массивов)
+//метод, создающий массив и выводящий на консоль исходный массив и массив произведений пар чисел
+int[] array = CreateArray(4, 0, 10);
+PrintResult(array);
+
+int[] array2 = CreateArray(5, 0, 10);
+PrintResult(array2);
