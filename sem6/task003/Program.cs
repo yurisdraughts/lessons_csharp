464e3b6650e8c2269676a69300c873ccaa62dfb4
Yuri M <yurisdraughts@gmail.com>
2022-08-22 21:50:11 +0300

    Added sem6/task003

---
+++ sem6/task003/Program.cs
@@ -0,0 +1,17 @@
+﻿void MakeBinary()
+{
+    Console.Write("Введите натуральное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    string binary = string.Empty;
+    int n = number;
+    while (n > 0)
+    {
+        binary = $"{n % 2}" + binary;
+        n /= 2;
+    }
+
+    Console.WriteLine($"{number} -> {binary}");
+}
+
+MakeBinary();
