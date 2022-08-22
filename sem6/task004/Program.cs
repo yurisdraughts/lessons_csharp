1b12c7241be01e26b3c7c7d838b36bd5e8d3a73a
Yuri M <yurisdraughts@gmail.com>
2022-08-22 21:53:18 +0300

    Added sem6/task004

---
+++ sem6/task004/Program.cs
@@ -0,0 +1,31 @@
+﻿string StringifyArray(int[] array)
+{
+    string stringArray = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) stringArray += $"{array[i]} ";
+        else stringArray += $"{array[i]}";
+    }
+    return stringArray;
+}
+
+int[] Fibonacci(int n)
+{
+    int[] fibs = new int[n];
+    fibs[0] = 0;
+    fibs[1] = 1;
+    for (int i = 2; i < fibs.Length; i++)
+    {
+        fibs[i] = fibs[i-2] + fibs[i-1];
+    }
+    return fibs;
+}
+
+void PrintResult(int n)
+{
+    Console.WriteLine($"Если N = {n} -> {StringifyArray(Fibonacci(n))}");
+}
+
+PrintResult(5);
+PrintResult(3);
+PrintResult(7);
