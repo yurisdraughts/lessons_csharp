58cf72a1daaf72a1895ff750fcc5ee8fb34172e6
Yuri M <yurisdraughts@gmail.com>
2022-08-22 21:01:47 +0300

    Added sem6/task002

---
+++ sem6/task002/Program.cs
@@ -0,0 +1,22 @@
+﻿bool CheckTriangleSides(int a, int b, int c)
+{
+    return a+b>c && a+c>b && b+c>a;
+}
+
+void IsTriangle()
+{
+    int[] triangle = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write($"Введите длину {i+1}-й стороны: ");
+        triangle[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    if (CheckTriangleSides(triangle[0],triangle[1],triangle[2]))
+    {
+        Console.WriteLine($"Треугольник с длинами сторон {triangle[0]}, {triangle[1]} и {triangle[2]} существует.");
+    }
+    else Console.WriteLine($"Треугольник с длинами сторон {triangle[0]}, {triangle[1]} и {triangle[2]} НЕ существует!");
+}
+
+IsTriangle();
