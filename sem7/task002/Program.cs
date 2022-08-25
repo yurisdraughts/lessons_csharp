37eb0ea0d66ad264d96ce451208571ceef44b2db
Yuri M <yurisdraughts@gmail.com>
2022-08-25 20:29:14 +0300

    Added sem7/task002

---
+++ sem7/task002/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = row + column;
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(10, 10);
+PrintMatrix(matrix);
