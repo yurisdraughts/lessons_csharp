cab477b3e1bf2f1836a3ab611797a74074077a8a
Yuri M <yurisdraughts@gmail.com>
2022-08-26 09:56:38 +0300

    Added homework7/task52

---
+++ homework7/task52/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] FindMeanValues(int[,] arr)
+{
+    double[] meanValues = new double[arr.GetLength(1)];
+    for (int column = 0; column < arr.GetLength(1); column++)
+    {
+        double sum = 0;
+        double mean = 0;
+
+        for (int row = 0; row < arr.GetLength(0); row++)
+        {
+            sum += arr[row, column];
+        }
+        mean = sum / arr.GetLength(0);
+        meanValues[column] = mean;
+    }
+    return meanValues;
+}
+
+string StringifyArray(double[] array)
+{
+    string result = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) result += array[i] + "; ";
+        else result += array[i];
+    }
+    return result;
+}
+
+void Main(int[,] array)
+{
+    PrintMatrix(array);
+    double[] meanValues = FindMeanValues(array);
+    Console.WriteLine($"Среднее арифметическое каждого столбца: {StringifyArray(meanValues)}");
+}
+
+int[,] matrix = CreateMatrix(4, 5, 0, 9);
+Main(matrix);
