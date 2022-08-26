d42d0b02922328308039e548e681ceee5cec1660
Yuri M <yurisdraughts@gmail.com>
2022-08-26 09:19:07 +0300

    Added homework7/task47

---
+++ homework7/task47/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateMatrix(int rowCount, int columnCount, double min, double max)
+{
+    double[,] array = new double[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            double random = new Random().NextDouble();
+            array[row, column] = random * (max - min) + min;
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(double[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Main(int rowCount, int columnCount, double min, double max)
+{
+    double[,] matrix = CreateMatrix(rowCount, columnCount, min, max);
+    PrintMatrix(matrix);
+}
+
+Main(3, 4, -9, 9);
