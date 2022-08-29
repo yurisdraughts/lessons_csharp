1e096d12b50ed7ddfc973854268af8fda390ed95
Yuri M <yurisdraughts@gmail.com>
2022-08-29 20:30:36 +0300

    Added sem8/task55

---
+++ sem8/task55/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] RotateMatrix(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            newMatrix[j, i] = matrix[i, j];
+        }
+    }
+
+    return newMatrix;
+}
+
+int[,] matrix = CreateMatrix(4, 7, 0, 100);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int[,] newMatrix = RotateMatrix(matrix);
+PrintMatrix(newMatrix);
