061f34488a9d3f9c6f8a38138f8e72ee22a2cf60
Yuri M <yurisdraughts@gmail.com>
2022-09-01 21:09:45 +0300

    modified sem9/task67

--- sem9/task67/Program.cs
+++ sem9/task67/Program.cs
@@ -4,7 +4,14 @@
     return DigitSum(number / 10, sum + number % 10);
 }
 
+int DigitSum2(int number)
+{
+    if (number == 0) return 0;
+    return (number % 10) + SumDigits2(number / 10);
+}
+
 Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int sum = DigitSum(number);
+//int sum = DigitSum(number);
+int sum = DigitSum2(number);
 Console.WriteLine($"Сумма цифр числа равна {sum}.");
