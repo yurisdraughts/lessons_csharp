97cf1b0b5fd834df00c1015631b3b5bb78562cc5
Yuri M <yurisdraughts@gmail.com>
2022-09-01 21:15:39 +0300

    Added sem9/task69

---
+++ sem9/task69/Program.cs
@@ -0,0 +1,15 @@
+﻿int Power(int a, int b)
+{
+    if (b == 1) return a;
+    if (b % 2 == 0) return Power(a*a, b/2);
+    return a * Power(a, b - 1);
+}
+
+Console.Write("Введите основание степени: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите показатель степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = Power(a, b);
+Console.WriteLine($"Результат равен {result}.");
