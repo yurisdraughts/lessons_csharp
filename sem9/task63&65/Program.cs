8bb435b0866db1ed785456e44d2412d98e1e57a0
Yuri M <yurisdraughts@gmail.com>
2022-09-01 20:42:22 +0300

    Added sem9/task63&65

---
+++ sem9/task63&65/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int n)
+{
+    if (n == 0) return;
+    PrintNumbers(n - 1);
+    Console.Write($"{n} ");
+}
+
+//PrintNumbers(number);
+
+void PrintNumbers2(int start, int end)
+{
+    if (start > end) return;
+    Console.Write($"{start} ");
+    PrintNumbers2(start + 1, end);
+}
+
+//PrintNumbers2(1, number);
+PrintNumbers2(3, number);
