15fa510e8b1462eafafba53443dd8e429c1bde19
Yuri M <yurisdraughts@gmail.com>
2022-09-02 09:40:39 +0300

    Added homework9/task66

---
+++ homework9/task66/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Sum(int start, int end)
+{
+    if (start == end) return end;
+    return start + Sum(start+1, end);
+}
+
+void Main()
+{
+    Console.Write("Введите первое число: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("Введите второе число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write($"Сумма чисел от {m} до {n} равна {Sum(m, n)}.");
+}
+
+Main();
