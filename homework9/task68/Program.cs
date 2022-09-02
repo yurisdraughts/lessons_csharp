6225a67eccd9274ab23c9a86b23a1a559d74fb03
Yuri M <yurisdraughts@gmail.com>
2022-09-02 09:57:41 +0300

    modified: homework9/task68

--- homework9/task68/Program.cs
+++ homework9/task68/Program.cs
@@ -1,8 +1,9 @@
-﻿//
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
 int Ackermann(int m, int n)
 {
     if (m == 0) return n+1;
-    else if (m > 0 && n == 0) return Ackermann(m-1, 1);
+    if (m > 0 && n == 0) return Ackermann(m-1, 1);
     return Ackermann(m-1, Ackermann(m, n-1));
 }
 
